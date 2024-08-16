dacb27c4bc2e7cb467075e3225dbf5cc523ee433
MNS <parsamanes@yahoo.com>
2024-08-16 10:17:20 +0300

    idfk

--- tailwind-flask-venv/app.py
+++ tailwind-flask-venv/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
-@app.route('/login', methods = ['POST', 'GET'])
+
+@app.route('/login')
 def login():
     return render_template('login.html')
 
